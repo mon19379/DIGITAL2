23a1c8186f33d01e56831dd24e24e431c8de2f44
mon19379 <mon19379@uvg.edu.gt>
2021-01-21 18:39:56 -0600

    OLA

---
+++ LAB1/LABD1.X/Carrera.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Carrera.c
+ * Author: franc
+ *
+ * Created on 21 de enero de 2021, 06:32 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ LAB1/LABD1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LABD1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LABD1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=labd1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/labd1.x.tar
